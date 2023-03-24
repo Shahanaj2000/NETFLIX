5406ce2e95edddcd21dade0f00d9b8c44a08b9bf
Shahanaj2000 <shahanajmt@gmail.com>
2023-03-24 23:19:56 +0530

    API Calling | apiKey generated | Models Created - @freezed

---
+++ lib/domain/core/api_end_point.dart
@@ -0,0 +1,7 @@
+//! Constant for Url - > TMDB
+import 'package:netflix/core/strings.dart';
+import 'package:netflix/infrastructure/api_key.dart';
+
+class ApiEndPoints {
+  static const  downloads = "$kBaseUrl/trending/all/day?api_key=$apiKey";
+}
---
+++ lib/domain/downloads/models/downloads.dart
@@ -0,0 +1,13 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'downloads.freezed.dart';
+part 'downloads.g.dart';
+@freezed
+class Downloads with _$Downloads{
+  const factory Downloads({
+    //! API name poster_path we want to chage that
+    @JsonKey(name: "poster_path") required String? posterPath,
+  }) = _Downloads;
+
+  factory Downloads.fromJson(Map<String, dynamic> json) => _$DownloadsFromJson(json);
+}
